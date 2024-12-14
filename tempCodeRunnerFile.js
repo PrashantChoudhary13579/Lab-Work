eddda88795715282a261933be72f891b24bd36e3
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2024-12-14 23:34:15 +0530

    practicing

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+
+// var m = prompt("Enter the number ");
+// // var m = 5;
+// let f = 1;
+// var n = m;
+// while (n != 0) {
+//     f = f*n;
+//     n--;
+// }
+// console.log("Factorial of " + m + " is "+ f);
